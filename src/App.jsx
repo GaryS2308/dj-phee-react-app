04ba0bfb2c7d543f2214d366d2e0c30dc6786d6d
GaryS2308 <garyjohnstrybis@gmail.com>
2025-08-06 16:38:05 +0200

    updated to routes to get response page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
+// App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import HeroSection from './components/pages/home/HeroSection';
 import PastEvents from './components/pages/past-events/past-events';
 import AboutPhee from './components/pages/about-phee/about-phee';
-import BookingForm from './components/pages/booking-form/BookingForm'
+import BookingForm from './components/pages/booking-form/BookingForm';
+import BookingResponse from './pages/BookingResponse'; // make sure this path is correct
 import './styles/styles.css';
+
+const HomePage = () => (
+  <>
+    <HeroSection />
+    <AboutPhee />
+    <PastEvents />
+    <BookingForm />
+  </>
+);
 
 const App = () => {
   return (
-    <div>
-      <HeroSection />
-      <AboutPhee />
-      <PastEvents />
-      <BookingForm />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/response" element={<BookingResponse />} />
+      </Routes>
+    </Router>
   );
 };
 
