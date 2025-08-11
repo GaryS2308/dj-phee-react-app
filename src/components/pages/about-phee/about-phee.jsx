79ef395a0895e7f38b5e188ac613dda4b68ce0ee
GaryS2308 <garyjohnstrybis@gmail.com>
2025-08-11 18:11:43 +0200

    mobile edits

--- src/components/pages/about-phee/about-phee.jsx
+++ src/components/pages/about-phee/about-phee.jsx
@@ -2,19 +2,26 @@ import React from 'react';
 import './about-phee.css';
 
 const AboutPhee = () => {
+  const images = [
+    { src: "https://res.cloudinary.com/dea6wzxd8/image/upload/v1754416287/phee2_ogysjy.jpg", alt: "DJPhee DJing" },
+    { src: "https://res.cloudinary.com/dea6wzxd8/image/upload/v1754416288/phee1_of7hnv.jpg", alt: "DJPhee at Event" },
+    { src: "https://res.cloudinary.com/dea6wzxd8/image/upload/v1754416288/phee4_dmdhvv.jpg", alt: "DJPhee Performing" },
+    { src: "https://res.cloudinary.com/dea6wzxd8/image/upload/v1754416286/phee5_uehcyi.png", alt: "DJPhee on Decks" },
+    { src: "https://res.cloudinary.com/dea6wzxd8/image/upload/v1754416289/phee3_g7ybop.jpg", alt: "DJPhee with Crowd" },
+    { src: "https://res.cloudinary.com/dea6wzxd8/image/upload/v1754907611/phee6_qfq0by.jpg", alt: "DJPhee in Action" }
+  ];
+
   return (
     <section id="about-phee">
       <h2>ABOUT PHEE</h2>
       <p>
         Phee is a versatile DJ rooted in the pulsating rhythms of Afrotech. With an ear for energy and a talent for reading any room, he brings unforgettable vibes to clubs, weddings, coffee shops, and festivals, effortlessly blending genres to suit every crowd.
       </p>
+
       <div className="phee-gallery">
-        <img src="https://res.cloudinary.com/dea6wzxd8/image/upload/v1754416287/phee2_ogysjy.jpg" alt="DJPhee DJing" />
-        <img src="https://res.cloudinary.com/dea6wzxd8/image/upload/v1754416288/phee1_of7hnv.jpg" alt="DJPhee at Event" />
-        <img src="https://res.cloudinary.com/dea6wzxd8/image/upload/v1754416288/phee4_dmdhvv.jpg" alt="DJPhee Performing" />
-        <img src="https://res.cloudinary.com/dea6wzxd8/image/upload/v1754416286/phee5_uehcyi.png" alt="DJPhee on Decks" />
-        <img src="https://res.cloudinary.com/dea6wzxd8/image/upload/v1754416289/phee3_g7ybop.jpg" alt="DJPhee with Crowd" />
-        <img src="https://res.cloudinary.com/dea6wzxd8/image/upload/v1754907611/phee6_qfq0by.jpg" alt="DJPhee in Action" />
+        {images.map((img, index) => (
+          <img key={index} src={img.src} alt={img.alt} />
+        ))}
       </div>
     </section>
   );
